c75d9ae1f72bae222e3b51f54090f60ca3cfee71
Bokkoa <krivozznova@gmail.com>
2022-10-11 22:43:20 -0500

    finishing job data

--- src/data/jobs.ts
+++ src/data/jobs.ts
@@ -4,7 +4,9 @@ interface IJob {
     company: string;
     from: string;
     to: string;
-    activities: IJobActivities[]
+    role: string;
+    companyLink: string;
+    activities: IJobActivities[];
 }
 
 interface IJobActivities{
@@ -13,12 +15,13 @@ interface IJobActivities{
 }
 
 export const jobs:IJob[] = [
-
     {
         id: 1,
         company: 'Continental',
         from: 'June 2019',
         to: 'April 2020',
+        role: 'Application Support Engineer',
+        companyLink: 'https://www.continental.com/es-mx/',
         activities: [
             {
                 description: "Application development and support for internal workflow areas improvement (HR, Finances, Local IT, etc)",
@@ -33,6 +36,129 @@ export const jobs:IJob[] = [
                 special: true
             }
         ]
-    }
+    },
+    {
+        id: 2,
+        company: 'UDG (Social Service)',
+        from: 'October 2019',
+        to: 'August 2020',
+        role: 'Fullstack Engineer',
+        companyLink: 'http://www.cucei.udg.mx/',
+        activities: [
+            {
+                description: "Fullstack development on course's platform for UDG staff, course's customization and offer of them.",
+                special: true
+            },
+            {
+                description: "Multi role access system.",
+                special: false,
+            },
+            {
+                description: "Fullstack development in Vue and Node",
+                special: false
+            }
+        ]
+    },
+    {
+        id: 3,
+        company: 'Roistom',
+        from: 'June 2020',
+        to: 'July 2021',
+        role: 'IT Professional',
+        companyLink: 'https://roistom.com/',
+        activities: [
+            {
+                description: "VPS and domains management.",
+                special: false
+            },
+            {
+                description: "Web maintance  and SQL service management for ConpaqI",
+                special: false,
+            },
+            {
+                description: "Technical support in hardware devices.",
+                special: false
+            }
+        ]
+    },
+    {
+        id: 4,
+        company: 'Contalite',
+        from: 'June 2020',
+        to: 'July 2021',
+        role: 'Fullstack developer',
+        companyLink: 'https://contalite.com/',
+        activities: [
+            {
+                description: "Fullstack development on Contalite app ( Invoice stamping (electronic tax validation), webhooks, electronic payments, mailing )  made with Laravel and React.",
+                special: true
+            },
+            {
+                description: "Coordination on project modules, team roles and activities asignment.",
+                special: false,
+            },
+            {
+                description: "Git versioning and  basic server configuration.",
+                special: false
+            }
+        ]
+    },
+    {
+        id: 5,
+        company: 'Altatec',
+        from: 'June 2021',
+        to: 'Current',
+        role: 'SemiSR Developer',
+        companyLink: 'https://altatec-de-occidente-sa-de-cv.negocio.site/',
+        activities: [
+            {
+                description: "Development on courses platform, made with Node and VueJs.",
+                special: false
+            },
+            {
+                description: "Support of quotation's software and development of pricelist microservice. Support of  .NET Core and development with Django and TDD Methodology",
+                special: true,
+            },
+            {
+                description: "Backend development for industrial dining room employee's petition's registry, API RESTful with NodeJS, NestJS and Lumen microservices. It erases the physical ticket cost and agilizes the dining room flow.",
+                special: true,
+            },
+            {
+                description: "Payroll kiosko support development, a simply application that print or send by email the employee's payroll file, made with NodeJS and React.",
+                special: false,
+            },
+            {
+                description: "Documental changes, an application for ISO requirements that allows to quality department agilizes its processes for employee's documentation. Made with Laravel for API rest and push notifications with Firebase service and aws S3 Storage integration",
+                special: false,
+            }
+        ]
+    },
+    {
+        id: 6,
+        company: 'Altatec',
+        from: 'June 2021',
+        to: 'Current',
+        role: 'Techlead Developer',
+        companyLink: 'https://altatec-de-occidente-sa-de-cv.negocio.site/',
+        activities: [
+            {
+                description: "Warehouse management system, made with microservices architecture in .NET Core, NodeJS and Lumen.",
+                special: true,
+            },
+            {
+                description: "RFID Middleware that allows Zebra and Honneywell readers connection. One of the features was the compatibility with another company applications as a plugin (microservice) made with Springboot and Vue",
+                special: true,
+            },
+            {
+                description: "Learning path's creation for each developer member focused on their role specialization.",
+                special: true,
+            },
+            {
+                description: "Architecture definition with a transition porpuse, it was segmented in different phases to reach an scalable architecture and multi-tenat SaaS software",
+                special: true,
+            },
+        ]
+    },
+
 
 ]

119bdc5850b77c4d976619be8989f40a3fad3502
Bokkoa <krivozznova@gmail.com>
2023-03-05 12:37:41 -0600

    fix(build): addis app css and js built

---
+++ dist/precache-manifest.76441115369147e8dead544aaa169207.js
@@ -0,0 +1,230 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c9efc2930ab9b3e38045",
+    "url": "/fc-site/css/app.85814dc9.css"
+  },
+  {
+    "revision": "12da76e01de8ca8f23d6",
+    "url": "/fc-site/css/chunk-vendors.71a233fb.css"
+  },
+  {
+    "revision": "95ab03c1829ed1930739a6710b144d4b",
+    "url": "/fc-site/docs/CV.pdf"
+  },
+  {
+    "revision": "0a40040123bddc14b0c590338dcb1792",
+    "url": "/fc-site/img/01.0a400401.png"
+  },
+  {
+    "revision": "400d18a4183d252a55c526e70abdc593",
+    "url": "/fc-site/img/01.400d18a4.png"
+  },
+  {
+    "revision": "4422fe1143f30b97fe58bf8ab5db4a8d",
+    "url": "/fc-site/img/01.4422fe11.png"
+  },
+  {
+    "revision": "535b9bfcd5e06a7a413e590340d5e290",
+    "url": "/fc-site/img/01.535b9bfc.png"
+  },
+  {
+    "revision": "5907af67d66461f0efabf21ba0d160ee",
+    "url": "/fc-site/img/01.5907af67.png"
+  },
+  {
+    "revision": "5ec20998e645ebf1478c7de3c99c3092",
+    "url": "/fc-site/img/01.5ec20998.png"
+  },
+  {
+    "revision": "608f8b5d26b6b2c78cb2fe013f263778",
+    "url": "/fc-site/img/01.608f8b5d.png"
+  },
+  {
+    "revision": "8d354a8b5321ec9deee26139ec0c45d7",
+    "url": "/fc-site/img/01.8d354a8b.png"
+  },
+  {
+    "revision": "fe050926e9a8f63651651c3ce5ca5588",
+    "url": "/fc-site/img/01.fe050926.png"
+  },
+  {
+    "revision": "04f10d8a8260722a9ec58e7945b70373",
+    "url": "/fc-site/img/02.04f10d8a.png"
+  },
+  {
+    "revision": "07f273525f4c8d9ed632b6281632b42c",
+    "url": "/fc-site/img/02.07f27352.png"
+  },
+  {
+    "revision": "47a2c128f1488acb4fb9ec4e487f41cd",
+    "url": "/fc-site/img/02.47a2c128.png"
+  },
+  {
+    "revision": "787d1bb5d6c2470d71e8c3e3f73a02a3",
+    "url": "/fc-site/img/02.787d1bb5.png"
+  },
+  {
+    "revision": "805a80ceab0e99614de988d6d99f8b65",
+    "url": "/fc-site/img/02.805a80ce.png"
+  },
+  {
+    "revision": "bfc59c03074bb7f4e68c7c710c93e0b3",
+    "url": "/fc-site/img/02.bfc59c03.png"
+  },
+  {
+    "revision": "fc34306eace9020079401058354b6887",
+    "url": "/fc-site/img/02.fc34306e.png"
+  },
+  {
+    "revision": "3440ee6075bd16025c1df954be1aebf8",
+    "url": "/fc-site/img/03.3440ee60.png"
+  },
+  {
+    "revision": "8620f8d4db241863163bfa167ba18e63",
+    "url": "/fc-site/img/03.8620f8d4.png"
+  },
+  {
+    "revision": "dcd95dc385230f32d3ecf345daacd696",
+    "url": "/fc-site/img/03.dcd95dc3.png"
+  },
+  {
+    "revision": "f5945d76ee4f9bed340a4d8a3a7867a8",
+    "url": "/fc-site/img/03.f5945d76.png"
+  },
+  {
+    "revision": "fd699813f1969684889980360b61a81f",
+    "url": "/fc-site/img/03.fd699813.png"
+  },
+  {
+    "revision": "fd939907134a9878ad6e03394fc34e79",
+    "url": "/fc-site/img/03.fd939907.png"
+  },
+  {
+    "revision": "1824f3c39792b484e77174f6c7672813",
+    "url": "/fc-site/img/04.1824f3c3.png"
+  },
+  {
+    "revision": "1a4a0ebe7d99a5fdf349203f4dca496b",
+    "url": "/fc-site/img/04.1a4a0ebe.png"
+  },
+  {
+    "revision": "1ed0698a3b95307c0b617032ee962e34",
+    "url": "/fc-site/img/04.1ed0698a.png"
+  },
+  {
+    "revision": "2740ec049fc109bb8541b5cb692e858e",
+    "url": "/fc-site/img/04.2740ec04.png"
+  },
+  {
+    "revision": "5ed23fc286b5f49cfd2038a2872d3d95",
+    "url": "/fc-site/img/04.5ed23fc2.png"
+  },
+  {
+    "revision": "73a70adc304a4b73f7b6eb5d5e388b62",
+    "url": "/fc-site/img/04.73a70adc.png"
+  },
+  {
+    "revision": "12405aae4fb2db45734cde3a1e262545",
+    "url": "/fc-site/img/05.12405aae.png"
+  },
+  {
+    "revision": "1b0357a641b383fdeda5c135d4963b06",
+    "url": "/fc-site/img/05.1b0357a6.png"
+  },
+  {
+    "revision": "77b217db9c60d9cb8d7031aa3b29bd5f",
+    "url": "/fc-site/img/05.77b217db.png"
+  },
+  {
+    "revision": "b3827afd2cc52be16abe6ea7c357a259",
+    "url": "/fc-site/img/05.b3827afd.png"
+  },
+  {
+    "revision": "48060b96be5f141ce91eb3ce0b8b2d75",
+    "url": "/fc-site/img/06.48060b96.png"
+  },
+  {
+    "revision": "816450599a0f1cdf9d5aecced626ebce",
+    "url": "/fc-site/img/06.81645059.png"
+  },
+  {
+    "revision": "c0c3333b3e67ed1c59e9d87de3284808",
+    "url": "/fc-site/img/06.c0c3333b.png"
+  },
+  {
+    "revision": "dfbac608997779fed3f5943814c4abdf",
+    "url": "/fc-site/img/06.dfbac608.png"
+  },
+  {
+    "revision": "1322d445467cfc097b01939cdb25130d",
+    "url": "/fc-site/img/07.1322d445.png"
+  },
+  {
+    "revision": "cbd36896257b58480bc802291dc84450",
+    "url": "/fc-site/img/07.cbd36896.png"
+  },
+  {
+    "revision": "ce0ec77b926f7799dde7f0f551ebf43a",
+    "url": "/fc-site/img/07.ce0ec77b.png"
+  },
+  {
+    "revision": "9d34d1587e8c21d4c7a63de09b81da2c",
+    "url": "/fc-site/img/08.9d34d158.png"
+  },
+  {
+    "revision": "abb2edb83167a92f60282b0cda984d23",
+    "url": "/fc-site/img/08.abb2edb8.png"
+  },
+  {
+    "revision": "4404eedae47d459e92c4d1c90889e3ac",
+    "url": "/fc-site/img/background01.4404eeda.jpg"
+  },
+  {
+    "revision": "ff7f6a2e595a724fdfa237815d17863a",
+    "url": "/fc-site/img/background02.ff7f6a2e.jpg"
+  },
+  {
+    "revision": "cd29e66f128191c03b00fa4e7b54754f",
+    "url": "/fc-site/img/background03.cd29e66f.jpg"
+  },
+  {
+    "revision": "572b900fb2df4b131547c9215ac73f6a",
+    "url": "/fc-site/img/github.572b900f.svg"
+  },
+  {
+    "revision": "ba6ea95dc78a3dfe1f5f828817b204d4",
+    "url": "/fc-site/img/gitlab.ba6ea95d.svg"
+  },
+  {
+    "revision": "697e79554f9d2aa2e226464b3481eb4f",
+    "url": "/fc-site/img/linkedin.697e7955.svg"
+  },
+  {
+    "revision": "6454bf54831bc8c5dee4a1a568efa70d",
+    "url": "/fc-site/img/logo.6454bf54.svg"
+  },
+  {
+    "revision": "25b7f95c99aa06cb3a6b5ac3541a27b5",
+    "url": "/fc-site/img/me.25b7f95c.jpg"
+  },
+  {
+    "revision": "fd5db6d5839271e0e101423ede9b5d01",
+    "url": "/fc-site/index.html"
+  },
+  {
+    "revision": "c9efc2930ab9b3e38045",
+    "url": "/fc-site/js/app.7f55ebaf.js"
+  },
+  {
+    "revision": "12da76e01de8ca8f23d6",
+    "url": "/fc-site/js/chunk-vendors.6e89c856.js"
+  },
+  {
+    "revision": "4c02c878ff80062a1f85538054f26140",
+    "url": "/fc-site/manifest.json"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/fc-site/robots.txt"
+  }
+]);
